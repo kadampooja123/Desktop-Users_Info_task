291e20f7c3e5ee02a3043fc1978b01dfe86d8da5
kadampooja123 <97834721+kadampooja123@users.noreply.github.com>
2022-01-16 18:27:35 +0530

    user commit

---
+++ User_Info.aspx
@@ -0,0 +1,152 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="User_Info.aspx.cs" Inherits="Users_Info_task._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>User Information</title>
+    <style type="text/css">
+        .style1 {
+            width: 286px;
+        }
+    </style>
+    <style type="text/css">
+   table {
+  background-color: #696969;
+  color: white;
+  
+  
+}
+
+btn_adduser 
+{  
+   background-color: #04AA6D;   
+   color: white;  
+}
+    </style>
+    
+</head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js" type="text/javascript"></script>
+<link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="Stylesheet" type="text/css" />
+<script type="text/javascript">
+    $(function () {
+        $("[id*=txtDate]").datepicker({
+            showOn: 'button',
+            buttonImageOnly: true,
+            buttonImage: 'images/calendar.png'
+        });
+    });
+</script>
+<script type="text/javascript">
+    $(function() {
+    $("[id*=txtdob1]").datepicker({
+            showOn: 'button',
+            buttonImageOnly: true,
+            buttonImage: 'images/calendar.png'
+        });
+    });
+</script>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table id="tab_user" >
+    <tr>
+   <td>
+       <asp:Label ID="lbl_Name" runat="server" Text="Name:"></asp:Label>
+        </td>
+        
+        <td class="style1">
+        <asp:TextBox ID="txt_Name" runat="server" ></asp:TextBox> 
+        </td>
+        </tr>
+        <tr>
+        <td>
+            <asp:Label ID="lbl_dob" runat="server" Text="DOB:"></asp:Label>
+        </td>
+        <td>
+        <asp:TextBox ID="txtDate" runat="server" ></asp:TextBox>
+        
+        </td></tr>
+        <tr>
+        <td>
+            <asp:Label ID="lbl_des" runat="server" Text="Designation:"></asp:Label>
+        </td>
+        <td class="style1">
+        <asp:TextBox ID="txt_des" runat="server" ></asp:TextBox> 
+        </td></tr>
+        <tr>
+        <td>
+            <asp:Label ID="lbl_skills" runat="server" Text="Skills"></asp:Label>
+        </td>
+        <td>
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+            <asp:ListItem >Effective communication</asp:ListItem>
+            <asp:ListItem >Teamwork</asp:ListItem>
+            <asp:ListItem >Problem Solving</asp:ListItem>
+            <asp:ListItem >Creativity</asp:ListItem>
+            <asp:ListItem >Time Management</asp:ListItem>
+            </asp:CheckBoxList>
+        </td></tr>
+        <tr>
+   <td></td>
+     <td>
+         <asp:Button ID="btn_adduser" runat="server" Text="Add User" Width="115px" OnClick="btn_adduser_Click"/>
+          <asp:Button runat="server" ID="btnUpdate" Text="Update" class="button button4" OnClick="btnUpdate_Click" Visible=false/>  
+          <asp:Button runat="server" ID="btnReset" Text="Reset"  class="button button4" OnClick="btnReset_Click"/>  
+                        
+      </td>
+        </tr>
+      </table>
+    </div>
+    
+    
+    
+    
+    <div>
+    <h4>Show User Date</h4>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false"  DataKeyNames="Id"
+ OnRowEditing="GridView1_RowEditing" OnRowCancelingEdit="OnRowCancelingEdit" 
+ OnRowDeleting="OnRowDeleting" EmptyDataText="No records has been added.">
+<Columns>
+<asp:TemplateField HeaderText="Id" Visible=false>  
+                                         <HeaderStyle HorizontalAlign="Left" />  
+                                        <ItemStyle HorizontalAlign="Left" />  
+                                        <ItemTemplate>  
+                                            <asp:Label runat="server" ID="lblId" Text='<%#Eval("Id") %>'></asp:Label>  
+                                        </ItemTemplate>  
+                                    </asp:TemplateField>
+    <asp:TemplateField HeaderText="Name" ItemStyle-Width="150">
+        <ItemTemplate>
+
+            <asp:Label ID="lblName" runat="server" Text='<%# Eval("Name") %>'></asp:Label>
+        </ItemTemplate>
+        
+    </asp:TemplateField>
+        <asp:TemplateField HeaderText="DOB" ItemStyle-Width="150">
+        <ItemTemplate>
+            <asp:Label ID="Lbldob1" runat="server" Text='<%# Eval("Dob") %>'></asp:Label>
+        </ItemTemplate>
+        
+    </asp:TemplateField>
+        <asp:TemplateField HeaderText="Designation" ItemStyle-Width="150">
+        <ItemTemplate>
+            <asp:Label ID="lbldes" runat="server" Text='<%# Eval("Designation") %>'></asp:Label>
+        </ItemTemplate>
+      
+    </asp:TemplateField>
+        <asp:TemplateField HeaderText="Skills" ItemStyle-Width="150">
+        <ItemTemplate>
+            <asp:Label ID="lbisk" runat="server" Text='<%# Eval("Skills") %>'></asp:Label>
+        </ItemTemplate>
+        
+    </asp:TemplateField>
+    
+    <asp:CommandField ButtonType="Link" ShowEditButton="true" ShowDeleteButton="true" ItemStyle-Width="150" ControlStyle-ForeColor="white"/>
+</Columns>
+</asp:GridView>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ User_Info.aspx.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+namespace Users_Info_task
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        private SqlCommand _sqlCommand;
+        private SqlDataAdapter _sqlDataAdapter;
+        DataSet _dtSet; 
+        public void CreateConnection()
+        {
+            SqlConnection _sqlConnection = new SqlConnection("Data Source=AMCCS-P-L-77;Initial Catalog =User_info_db;User ID=sa;Password=APP@2k21;Connect Timeout=360000");
+            _sqlCommand = new SqlCommand();
+            _sqlCommand.Connection = _sqlConnection;
+        }
+        public void OpenConnection()
+        {
+            _sqlCommand.Connection.Open();
+        }
+        public void CloseConnection()
+        {
+            _sqlCommand.Connection.Close();
+        }
+        public void DisposeConnection()
+        {
+            _sqlCommand.Connection.Dispose();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindGrid_User_data();
+        }
+
+        private static void ShowAlertMessage(string error)
+        {
+            System.Web.UI.Page page = System.Web.HttpContext.Current.Handler as System.Web.UI.Page;
+            if (page != null)
+            {
+                error = error.Replace("'", "\'");
+                System.Web.UI.ScriptManager.RegisterStartupScript(page, page.GetType(), "err_msg", "alert('" + error + "');", true);
+            }
+        }
+
+        public void ClearControls()
+        {
+            txt_Name.Text = "";
+            txtDate.Text = "";
+            txt_des.Text = "";
+            CheckBoxList1.ClearSelection();
+
+        }
+
+
+        protected void btn_adduser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CreateConnection();
+                OpenConnection();
+                _sqlCommand.CommandText = "user_crud_operation";
+                _sqlCommand.CommandType = CommandType.StoredProcedure;
+                _sqlCommand.Parameters.AddWithValue("@Action", "INSERT");
+                _sqlCommand.Parameters.AddWithValue("@Name", Convert.ToString(txt_Name.Text));
+                _sqlCommand.Parameters.AddWithValue("@Dob", (txtDate.Text));
+                _sqlCommand.Parameters.AddWithValue("@Designation", Convert.ToString(txt_des.Text));
+                string lst = "";
+
+                for (int i = 0; i < CheckBoxList1.Items.Count; i++)
+                {
+                    if (CheckBoxList1.Items[i].Selected)
+                    {
+                        lst = lst + "'" + CheckBoxList1.Items[i].Value + "' ,";
+                    }
+                }
+                _sqlCommand.Parameters.AddWithValue("@Skills", Convert.ToString(lst));
+
+                int result = Convert.ToInt32(_sqlCommand.ExecuteNonQuery());
+
+
+                ShowAlertMessage("Record Is Inserted Successfully");
+                BindGrid_User_data();
+                ClearControls();
+
+            }
+            catch (Exception ex)
+            {
+
+                ShowAlertMessage("Check your input data");
+
+            }
+            finally
+            {
+                CloseConnection();
+                DisposeConnection();
+            }
+        }
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            ClearControls();
+        } 
+
+        private void BindGrid_User_data()
+        {
+            
+
+
+           
+                CreateConnection();
+                OpenConnection();
+                _sqlCommand.CommandText = "user_crud_operation_select";
+                _sqlCommand.CommandType = CommandType.StoredProcedure;
+                _sqlCommand.Parameters.AddWithValue("@Action", "SELECT");
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    sda.SelectCommand = _sqlCommand;
+
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        GridView1.DataSource = dt;
+                        GridView1.DataBind();
+                    }
+                }
+          
+           
+                CloseConnection();
+                DisposeConnection();
+            
+
+        }
+
+
+
+
+        protected void OnRowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            this.BindGrid_User_data();
+        }
+
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            btn_adduser.Visible = false;
+            btnUpdate.Visible = true;
+
+            int RowIndex = e.NewEditIndex;
+            Label empid = (Label)GridView1.Rows[RowIndex].FindControl("lblId");
+            Session["id"] = empid.Text;
+
+            txt_Name.Text = ((Label)GridView1.Rows[RowIndex].FindControl("lblName")).Text.ToString();
+            txtDate.Text = ((Label)GridView1.Rows[RowIndex].FindControl("Lbldob1")).Text.ToString();
+            txt_des.Text = ((Label)GridView1.Rows[RowIndex].FindControl("lbldes")).Text.ToString();
+            CheckBoxList1.Text = ((Label)GridView1.Rows[RowIndex].FindControl("lbisk")).Text.ToString();
+
+        }
+
+
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        { 
+             try  
+    {  
+               
+        CreateConnection();  
+        OpenConnection(); 
+                   
+                    _sqlCommand.CommandText = "user_crud_operation";
+                    _sqlCommand.CommandType = CommandType.StoredProcedure;
+                    _sqlCommand.Parameters.AddWithValue("@Action", "UPDATE");
+                    _sqlCommand.Parameters.AddWithValue("@Name", Convert.ToString(txt_Name.Text));
+                    _sqlCommand.Parameters.AddWithValue("@Dob", (txtDate.Text));
+                    _sqlCommand.Parameters.AddWithValue("@Designation", Convert.ToString(txt_des.Text));
+                    string lst = "";
+
+                    for (int i = 0; i < CheckBoxList1.Items.Count; i++)
+                    {
+                        if (CheckBoxList1.Items[i].Selected)
+                        {
+                            lst = lst + "'" + CheckBoxList1.Items[i].Value + "' ,";
+                        }
+                    }
+                    _sqlCommand.Parameters.AddWithValue("@Skills", Convert.ToString(lst));
+                    _sqlCommand.Parameters.AddWithValue("@Id", Convert.ToDecimal(Session["Id"]));  
+  
+
+                    int result = Convert.ToInt32(_sqlCommand.ExecuteNonQuery());
+                  
+                        ShowAlertMessage("Record Is Updated Successfully");
+                        GridView1.EditIndex = -1;
+                        BindGrid_User_data();
+                        ClearControls();
+                  
+    }
+
+             catch (Exception ex)
+             {
+                 ShowAlertMessage("Check your input data");
+             }
+             finally
+             {
+                 CloseConnection();
+                 DisposeConnection();
+             }  
+                
+            
+           
+        }
+
+
+
+        protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int Id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            CreateConnection();
+            
+                using (SqlCommand cmd = new SqlCommand("user_crud_operation"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Action", "DELETE");
+                    cmd.Parameters.AddWithValue("@Id", Id);
+                    OpenConnection();
+                    cmd.ExecuteNonQuery();
+                    CloseConnection();
+                   
+                }
+            
+            this.BindGrid_User_data();
+
+
+
+        }
+
+
+
+        protected void OnRowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindGrid_User_data();
+        }  
+    }
+
+
+}
